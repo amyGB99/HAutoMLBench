c17e698e9a846fcc8e7f4eafca74e7b6c52e837b
Amy <borrellamanda@gmail.com>
2022-10-18 09:49:39 -0400

    add sst-en dataset

--- source/datasets/sst_en.py
+++ source/datasets/sst_en.py
@@ -6,7 +6,7 @@ import utils
 from sklearn.metrics import f1_score
 import numpy as np 
 
-a = Dataset("sst-en", "https://github.com/amyGB99/automl_benchmark/releases/download/paws-x/paws-x-es.zip", load_sst_en )
+a = Dataset("sst-en", "https://github.com/amyGB99/automl_benchmark/releases/download/sst-en/sst-en.zip", load_sst_en )
 c = utils.load_dataset_definition("sst-en")
 Xtr,ytr,Xde,yde,Xte,yte = c.loader_func(a)
 ytr1= np.asarray(ytr)
@@ -24,8 +24,7 @@ print(yte1)
 print("aqui")
 y = automl.predict(Xte)
 print(y)
-for i in y:  
-  print(i)
+
 print(automl.best_score_)
 score = automl.score(Xte,yte1)
 
